44c8c3c0a5b484030c64d386526f2fb2ead4336d
dudu7615 <dudu7615@qq.com>
2024-11-18 20:10:27 +0800

    种花

---
+++ 种花/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("in", "r", stdin);
+    int T, id;
+    cin >> T >> id;
+    for (; T--; T > 0)
+    {
+        int n, m, c, f;
+        cin >> n >> m >> c >> f;
+        vector<string> str(n,string(m,' '));
+
+    }
+}
