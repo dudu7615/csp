53fe61ae89607fa1aa4e377aabcd1fb5f8fe30e2
dudu7615 <dudu7615@qq.com>
2024-12-13 20:27:52 +0800

    迷宫的所有路径

--- 迷宫的所有路径/main.cpp
+++ 迷宫的所有路径/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-void findPaths(int x, int y, int N, vector<vector<bool>>& visited, string path, vector<string>& paths) {
+void findPaths(int x, int y, int N, vector<vector<bool>> &visited, string path,
+               vector<string> &paths) {
     if (x == N - 1 && y == N - 1) {
         paths.push_back(path);
         return;
@@ -14,19 +15,25 @@ void findPaths(int x, int y, int N, vector<vector<bool>>& visited, string path, 
 
     // Right
     if (y + 1 < N && !visited[x][y + 1]) {
-        findPaths(x, y + 1, N, visited, path + "->" + to_string(x + 1) + "," + to_string(y + 2), paths);
+        findPaths(x, y + 1, N, visited,
+                  path + "->" + to_string(x + 1) + "," + to_string(y + 2),
+                  paths);
     }
     // Down
     if (x + 1 < N && !visited[x + 1][y]) {
-        findPaths(x + 1, y, N, visited, path + "->" + to_string(x + 2) + "," + to_string(y + 1), paths);
+        findPaths(x + 1, y, N, visited,
+                  path + "->" + to_string(x + 2) + "," + to_string(y + 1),
+                  paths);
     }
     // Left
     if (y - 1 >= 0 && !visited[x][y - 1]) {
-        findPaths(x, y - 1, N, visited, path + "->" + to_string(x + 1) + "," + to_string(y), paths);
+        findPaths(x, y - 1, N, visited,
+                  path + "->" + to_string(x + 1) + "," + to_string(y), paths);
     }
     // Up
     if (x - 1 >= 0 && !visited[x - 1][y]) {
-        findPaths(x - 1, y, N, visited, path + "->" + to_string(x) + "," + to_string(y + 1), paths);
+        findPaths(x - 1, y, N, visited,
+                  path + "->" + to_string(x) + "," + to_string(y + 1), paths);
     }
 
     visited[x][y] = false;
