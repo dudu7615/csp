e8bda69d5afba12f193374bd48e1527edfb22e1f
dudu7615 <dudu7615@qq.com>
2025-07-16 22:49:07 +0800

    1264：【例9.8】合唱队形

---
+++ 1264：【例9.8】合唱队形/main.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+using vi = vector<int>;
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ifstream in("in");
+    cin.rdbuf(in.rdbuf());
+
+    int N;
+    cin >> N;
+    vi T(N);
+    for(auto &t : T) {
+        cin >> t;
+    }
+
+    return 0;
+}
